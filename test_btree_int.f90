eb647ae13c660316d425125deae5ab7b4842f918
Hongding Gao <hongdinggao@gmail.com>
2020-02-13 20:57:02 +0100

    test of timing

--- test_btree_int.f90
+++ test_btree_int.f90
@@ -5,10 +5,10 @@ use mod_rowcol
 implicit none
 type(thetree) :: mytree
 integer(int64) :: i
-!integer, allocatable :: A(:)
+integer(int64), allocatable :: A(:)
 !real, allocatable :: A(:)
 !character(len=8), allocatable :: A(:)
-type(rowcol), allocatable :: A(:)
+!type(rowcol), allocatable :: A(:)
 real, allocatable :: B(:)
 real(real64) :: t0, t1
 !A = [5,6,21,24,25,65,75,81,85,95]
@@ -17,8 +17,7 @@ real(real64) :: t0, t1
 !    "Fuguang3", "Giaolei4", "HaoQin88", "HbiYan77", "Hiaoming"]
 allocate(A(10))
 do i=1, 10
-   A(i)%row = i
-   A(i)%col = i
+   A(i) = i
 end do
 
 B = [1.11, 2.22, 3.33, 4.44, 5.55, 6.66, 7.77, 8.88, 9.99, 10.10]
@@ -27,18 +26,24 @@ print *, "The vector before sorting "
 !write(*, '(*(i0, 1x))') A
 !write(*, '(*(f8.3, 1x))') A
 !write(*, '(*(a, 1x))') A
-!write(*, '(*(g0, 1x))') A
+write(*, '(*(g0, 1x))') A
 
 ! constructor
 !mytree = thetree()
 
+call cpu_time(t0)
 do i=1, size(A)
     !mytree = A(i)
     call mytree%add(A(i), B(i))
-    print*, mytree%has_key(A(i))
+    !print*, mytree%has_key(A(i))
 end do
+call cpu_time(t1)
+print*, "Time of setting up the tree ", t1 - t0
 
-print*, mytree%has_key("Hongding")
+call cpu_time(t0)
+print*, mytree%has_key(A(6))
+call cpu_time(t1)
+print*, "Time of check the key ", t1 - t0
 
 print*, "Show the tree "
 print *
